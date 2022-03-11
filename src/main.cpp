5f15856d22b59bb51c995e0bbc149f5e33ba9134
Lowiek Van den Stockt <lowiek@fablabfactory.com>
2022-03-11 11:31:15 +0100

    Refactored layers a bit

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,24 @@
-#include "Constants.h"
+#include "Layers/Layer.h"
+#include "Layers/LayerStack.h"
+#include "Layers/PhysicalLayer.h"
+#include "Layers/TransportLayer.h"
+#include "Layers/EncryptionLayer.h"
+#include "Layers/ApplicationLayer.h"
+#include "Encryption/EncryptionType.h"
 
-#define SENDER 1
-#define RECEIVER 2
+LayerStack networkStack;
 
-#define DEVICE RECEIVER 
+ApplicationLayer applicationLayer;
+void setup()
+{
 
-#if TARGET == ESP_32
-  #if DEVICE == SENDER
-    #include "./ESPSender.h"
-  #endif
+  // Setup NetworkStack
+  networkStack.addLayer(new PhysicalLayer(433E6));
+  networkStack.addLayer(new TransportLayer());
+  networkStack.addLayer(new EncryptionLayer(ENC_AES));
+  networkStack.addLayer(&applicationLayer);
+}
 
-  #if DEVICE == RECEIVER
-    #include "./ESPReceiver.h"
-  #endif
-#elif TARGET == WINDOWS
-  #include "./Test.h"
-#endif
-
-
-// #include <stdint.h>
-// #include "Constants.h"
-// #include "./Encryption/AES.h"
-// #include "Utils.h"
-
-// int main(){
-//     uint8_t buffer[16] = {0x0,0x1,0x2,0x3,0x4,5,6,7,8,9,10,11,12,13,14,15};
-//     int buffer_size = 16;
-
-//     uint8_t key[16] = AES_KEY;
-//     uint8_t init_vector[16] = AES_IV;
-
-//     AES aes(AESKeyLength::AES_128);
-//     uint8_t* cypher_text = aes.EncryptCBC(&buffer[0], buffer_size, &key[0], &init_vector[0]);
-
-//     uint8_t key2[16] = AES_KEY;
-//     uint8_t init_vector2[16] = AES_IV;
-
-//     uint8_t* plain_text = aes.DecryptCBC(&cypher_text[0], buffer_size, &key2[0], &init_vector2[0]);
-//     printArray(plain_text, 16);
-// }
+void loop()
+{
+}
