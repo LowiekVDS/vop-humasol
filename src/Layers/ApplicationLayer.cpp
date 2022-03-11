8777f1135fa7ca2c85052841156de265b02a3900
romaes <Robin.Maes@UGent.be>
2022-03-11 11:59:55 +0100

    Added extraction of Entries in Application Layer

--- src/Layers/ApplicationLayer.cpp
+++ src/Layers/ApplicationLayer.cpp
@@ -2,10 +2,32 @@
 
 void ApplicationLayer::up(uint8_t *payload, uint8_t length)
 {
-    // Process packet here!
+    extractEntries(payload, length);
+
+    for (auto it = m_entries.begin(); it != m_entries.end();)
+    {
+        TLVEntry *entry = *it;
+        entry->process(); // This should be implemented in each Entry class and dispatch its changes to the appropriate handler
+        it = m_entries.erase(it);
+    }
 }
 
 void ApplicationLayer::down(uint8_t *payload, uint8_t length)
 {
     downLayer->down(payload, length);
+}
+
+void ApplicationLayer::extractEntries(uint8_t *payload, uint8_t length) {
+    
+    uint8_t *pointer = &payload[0];
+
+    while ((pointer - payload) < length)
+    {
+        if (!isType(*pointer))
+            break;
+
+        TLVEntry *e = TLVEntry::CreateFromType(*pointer);
+        e->decode(pointer);
+        m_entries.push_back(e);
+    }
 }
--- src/Layers/ApplicationLayer.h
+++ src/Layers/ApplicationLayer.h
@@ -1,8 +1,14 @@
 #pragma once
 #include "Layer.h"
+#include "../Entries/TLVEntry.h"
+
 
 class ApplicationLayer : public Layer {
+    private:
+        std::vector<TLVEntry*> m_entries;
     public:
         void up(uint8_t *payload, uint8_t length) override;
         void down(uint8_t *payload, uint8_t length) override;
+    private:
+        void extractEntries(uint8_t *payload, uint8_t length);
 };
