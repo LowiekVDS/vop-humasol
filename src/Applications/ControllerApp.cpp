cf309c3a2a6c6e19a0619fd955ea88f31ee91357
Lowiek Van den Stockt <lowiekvds@gmail.com>
2022-05-05 14:31:30 +0200

    bump

--- src/Applications/ControllerApp.cpp
+++ src/Applications/ControllerApp.cpp
@@ -1,7 +1,8 @@
 #include "ControllerApp.h"
 #include "Arduino.h"
 #include <LoRa.h>
-void ControllerApp::up(uint8_t* payload, uint8_t length) {
+void ControllerApp::up(uint8_t *payload, uint8_t length)
+{
     std::vector<TLVEntry *> entries = ApplicationLayer::extractEntries(payload, length);
 
     for (auto it = entries.begin(); it != entries.end();)
@@ -10,23 +11,31 @@ void ControllerApp::up(uint8_t* payload, uint8_t length) {
         Serial.println("Received Entry");
 
         // Pump State Entry
-        if (entry->type == PUMP_STATE){
-            PumpStateEntry* pumpStateEntry = (PumpStateEntry*)entry;
+        if (entry->type == PUMP_STATE)
+        {
+            PumpStateEntry *pumpStateEntry = (PumpStateEntry *)entry;
 
-            if (pumpStateEntry->state == PUMP_OPEN){
+            if (pumpStateEntry->state == PUMP_OPEN)
+            {
                 this->m_newPumpState = PumpState::OPEN;
-            } else if (pumpStateEntry->state==PUMP_CLOSE){
+            }
+            else if (pumpStateEntry->state == PUMP_CLOSE)
+            {
                 this->m_newPumpState = PumpState::CLOSED;
             }
         }
 
         // Pump State Entry
-        else if (entry->type == PUMP_LEVEL){
-            PumpLevelEntry* pumpLevelEntry = (PumpLevelEntry*)entry;
+        else if (entry->type == PUMP_LEVEL)
+        {
+            PumpLevelEntry *pumpLevelEntry = (PumpLevelEntry *)entry;
 
-            if (pumpLevelEntry->level > 90){
+            if (pumpLevelEntry->level > 90)
+            {
                 this->m_newPumpState = PumpState::CLOSED;
-            } else {
+            }
+            else
+            {
                 this->m_newPumpState = PumpState::OPEN;
             }
         }
@@ -34,28 +43,9 @@ void ControllerApp::up(uint8_t* payload, uint8_t length) {
     }
 }
 
-bool ControllerApp::step() {
+bool ControllerApp::step()
+{
     LoRa.receive();
-    // Serial.println("Step Controller");
-
-    // if (this->m_newPumpState != this->m_pumpState){
-    //     if (m_newPumpState == PumpState::OPEN){
-    //         if (m_pumpState == PumpState::OPEN){
-    //             Serial.println("Still Pumping");
-    //         } else {
-    //             Serial.println("Started Pumping");
-    //         }
-    //     } else if (m_newPumpState == PumpState::CLOSED){
-    //         if (m_pumpState == PumpState::CLOSED){
-    //             Serial.println("Still not Pumping");
-    //         } else {
-    //             Serial.println("Stopped Pumping");
-    //         }
-    //     }
-    //     this->m_pumpState = this->m_newPumpState;
-    //     digitalWrite(GPIO_NUM_34, m_pumpState);
-    // }
-
     digitalWrite(GPIO_NUM_32, !this->m_newPumpState);
     return true;
 }
--- src/Applications/ControllerApp.h
+++ src/Applications/ControllerApp.h
@@ -7,12 +7,20 @@ class ControllerApp : public Application {
         OPEN = LOW,
         CLOSED = HIGH
     };
-
+    
     public:
+        uint16_t prm_pin_pump;
+        unsigned long prm_safety_mode_init_timeout;
+        unsigned long prm_safety_mode_pumping_period;
+        unsigned long prm_safety_mode_resting_period;
+        bool prm_invert;
+
+        bool safety_mode;
+
         PumpState m_pumpState;
         PumpState m_newPumpState;
     public:
-        ControllerApp() : Application() {};
+        ControllerApp() : Application(), prm_pin_pump(GPIO_NUM_32), prm_safety_mode_init_timeout(60000), prm_safety_mode_pumping_period(60000), prm_safety_mode_resting_period(60 * 60000) {};
         ~ControllerApp() {};
     public:
         void up(uint8_t* payload, uint8_t length) override;
